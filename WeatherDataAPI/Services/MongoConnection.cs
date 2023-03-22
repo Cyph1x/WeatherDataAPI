273a8f8a1087a2ac2140ef7dd33a3c2bb9f2430c
joshua ward <joshuaward1000@gmail.com>
2023-03-22 17:11:10 +1000

    try to use environment variable

--- WeatherDataAPI/Services/MongoConnection.cs
+++ WeatherDataAPI/Services/MongoConnection.cs
@@ -10,9 +10,14 @@ namespace WeatherDataAPI.Services
         private string _databaseName;
         public MongoConnection(IOptions<DefaultMongoConnection> mongoConnection)
         {
-            _connectionString = mongoConnection.Value.ConnectionString;
+                _connectionString = mongoConnection.Value.ConnectionString;
                 _databaseName = mongoConnection.Value.DatabaseName;
-            
+                if (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME")))
+                {
+                    //operating in a cloud environment
+                    _connectionString = Environment.GetEnvironmentVariable("mongoConnection");
+                    _databaseName = Environment.GetEnvironmentVariable("mongoCollection");
+                }
         }
 
         public IMongoDatabase GetDatabase()
